aa025b225fae0ad9a0069cfbc34b0bdf0dd0b0a0
liling yang <yangll2525@163.com>
2024-01-22 17:19:28 -0800

    Fix Initialized problem

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'firebase_options.dart';
 import './login_page.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
