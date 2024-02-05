4da3fd14ffa9a9469af2307373217eabaf231a36
liling yang <yangll2525@163.com>
2024-02-04 19:54:23 -0800

    User List 1.Get all users from Cloud Firestore 2.Modify ui

--- lib/all_profiles_page.dart
+++ lib/all_profiles_page.dart
@@ -1,76 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_page.dart';
+import 'db.dart';
 
-class AllProfilesPage extends StatefulWidget {
+class AllProfilesPage extends StatelessWidget {
   const AllProfilesPage({super.key});
 
+  // This widget is the root of your application.
   @override
-  State<StatefulWidget> createState() {
-    return _AllProfilesPageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bounty Romance',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AllProfiles(title: 'Welcome!'),
+    );
   }
 }
 
-class _AllProfilesPageState extends State<AllProfilesPage> {
-  // some data structure to hold the user profile data from firebase
-  List<String> allProfiles = [];
+class AllProfiles extends StatefulWidget {
+  const AllProfiles({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<AllProfiles> createState() => _AllProfilesState();
+}
+
+class _AllProfilesState extends State<AllProfiles> {
+  List<Map<String, dynamic>> userList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getAllUsers();
+  }
+
+  Future<void> getAllUsers() async {
+    List<Map<String, dynamic>> usersData = await FireStoreService.getUsers();
+    setState(() {
+      userList = usersData;
+    });
+  }
+
+  Future<void> logoutFn(context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const Login(title: 'Bounty Romance')));
+  }
 
   @override
   Widget build(BuildContext context) {
-    final double width = MediaQuery.of(context).size.width; //to get the width of screen
-    final double height = MediaQuery.of(context).size.height; //to get height of screen
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.topCenter,
-                height: height * 0.45,
-                child: Stack(
-                  children: [
-                    Positioned(
-                        child: Material(
-                          child: Container(
-                            height: height*0.4,
-                            width: width*0.95,
-                            decoration: BoxDecoration(
-                              color: Colors.amber,
-                              borderRadius: BorderRadius.circular(0.0),
-                              boxShadow: const [
-                                BoxShadow(
-                                  color: Colors.grey,
-                                  offset: Offset(
-                                    5.0,
-                                    5.0,
-                                  ),
-                                  blurRadius: 10.0,
-                                  spreadRadius: 2.0,
-                                ), //BoxShadow//BoxShadow
-                              ],
-                            ),
-                          ),
-
-                        )
-                    ),
-                    Container(
-                      height: 200,
-                      width: 200,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        image: const DecorationImage(
-                          fit: BoxFit.fill,
-                          image: AssetImage("assets/brad-pitt-attends-the-premiere-of-20th-century-foxs--square.jpg"),
-                        ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: () {
+              logoutFn(context);
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: userList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              elevation: 3,
+              margin: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset('assets/default.jpg', width: 60, height: 60),
+                  ),
+                  Text(userList[index]['name'], style: const TextStyle(fontSize: 18)),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: IconButton(
+                        icon: const Icon(Icons.add),
+                        onPressed: () {
+                          print("send like request");
+                        },
                       ),
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
+                  ),
+                ],
+              ),
+            );
+          }
       ),
     );
   }
-
 }
