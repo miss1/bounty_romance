470f93aa833f0c7305fcf9301392a6745dde9b6f
Sizhe Liu <sliu1@seattleu.edu>
2024-02-26 17:52:59 -0800

    added pageview logic to all_profiles_page.dart

--- lib/all_profiles_page.dart
+++ lib/all_profiles_page.dart
@@ -15,6 +15,7 @@ class AllProfilesPage extends StatefulWidget {
 class _AllProfilesState extends State<AllProfilesPage> {
   List<UserInfoModel> userList = [];
   bool filledSelected = false;
+  final PageController controller = PageController();
 
   @override
   void initState() {
@@ -35,13 +36,63 @@ class _AllProfilesState extends State<AllProfilesPage> {
 
   Widget _imageWidget(avatar) {
     if (avatar != '') {
-      return Image.network(avatar);
+      return Image.network(avatar, width: MediaQuery.of(context).size.width * 0.9, fit: BoxFit.cover,);
     }
-    return Image.asset('assets/default.jpg');
+    return Image.asset('assets/default.jpg', width: MediaQuery.of(context).size.width * 0.9, fit: BoxFit.cover,);
   }
 
   @override
   Widget build(BuildContext context) {
+    return PageView(
+      controller: controller,
+      children: userList.map((e) => Center(
+        child: GestureDetector(
+          onTap: () {
+            GoRouter.of(context).push('/userProfile', extra: e.id);
+          },
+          child: Card(
+            elevation: 3.0,
+            margin: const EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: _imageWidget(e.avatar)),
+                  ),
+                const SizedBox(height: 10),
+                Center(
+                  child: Wrap(
+                    children: [
+                      Text(
+                        e.name,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                            fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      const SizedBox(width: 10,),
+                      Text(
+                        e.age,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      )).toList(),
+    );
+
+
     return Container(
       color: Colors.white,
       child: ListView.builder(
