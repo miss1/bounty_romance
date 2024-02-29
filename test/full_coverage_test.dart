417ee3ee858bd1eeb0bda7f0ac5dec61f7459fbf
liling yang <yangll2525@163.com>
2024-02-29 10:53:04 -0800

    add test for map page and user profile page

--- test/full_coverage_test.dart
+++ test/full_coverage_test.dart
@@ -3,7 +3,9 @@ import 'package:bounty_romance/common/data.dart';
 import 'package:bounty_romance/common/firebase_options.dart';
 import 'package:bounty_romance/common/router.dart';
 import 'package:bounty_romance/common/nav_notifier.dart';
+import 'package:bounty_romance/edit_profile_page.dart';
 import 'package:bounty_romance/widget_profile.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
@@ -17,6 +19,7 @@ import 'package:bounty_romance/registration_page.dart';
 import 'package:bounty_romance/upload_image_page.dart';
 import 'package:bounty_romance/home_nav_bar.dart';
 import 'package:bounty_romance/user_profile_page.dart';
+import 'package:bounty_romance/map_page.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -24,6 +27,21 @@ import 'full_coverage_test.mocks.dart';
 
 // Mock Firebase
 @GenerateMocks([FireStoreService])
+
+UserInfoModel getMockUserInfo () {
+  return UserInfoModel(
+      id: '0',
+      name: 'Kimmy',
+      email: 'test1@gmail.com',
+      age: '29',
+      intro: 'love me',
+      gender: 0,
+      avatar: '',
+      city: '',
+      lat: 0.0,
+      lng: 0.0
+  );
+}
 
 void main() {
   // Create a mock FirebaseAuth instance
@@ -155,27 +173,16 @@ void main() {
 
   test('UserInfoModel Test', () {
     // create a UserInfoModel instance
-    UserInfoModel userInfo = UserInfoModel(
-      id: '1',
-      name: 'John',
-      email: 'john@example.com',
-      age: '30',
-      intro: 'A brief intro',
-      gender: 1,
-      avatar: 'avatar_url',
-      city: 'New York',
-      lat: 0.0,
-      lng: 0.0
-    );
+    UserInfoModel userInfo = getMockUserInfo();
 
-    expect(userInfo.id, '1');
-    expect(userInfo.name, 'John');
-    expect(userInfo.email, 'john@example.com');
-    expect(userInfo.age, '30');
-    expect(userInfo.intro, 'A brief intro');
-    expect(userInfo.gender, 1);
-    expect(userInfo.avatar, 'avatar_url');
-    expect(userInfo.city, 'New York');
+    expect(userInfo.id, '0');
+    expect(userInfo.name, 'Kimmy');
+    expect(userInfo.email, 'test1@gmail.com');
+    expect(userInfo.age, '29');
+    expect(userInfo.intro, 'love me');
+    expect(userInfo.gender, 0);
+    expect(userInfo.avatar, '');
+    expect(userInfo.city, '');
 
     // test generateEmpty function
     UserInfoModel emptyUserInfo = UserInfoModel.generateEmpty();
@@ -190,14 +197,14 @@ void main() {
 
     // test toMap function
     Map<String, dynamic> userInfoMap = userInfo.toMap();
-    expect(userInfoMap['id'], '1');
-    expect(userInfoMap['name'], 'John');
-    expect(userInfoMap['email'], 'john@example.com');
-    expect(userInfoMap['age'], '30');
-    expect(userInfoMap['intro'], 'A brief intro');
-    expect(userInfoMap['gender'], 1);
-    expect(userInfoMap['avatar'], 'avatar_url');
-    expect(userInfoMap['city'], 'New York');
+    expect(userInfoMap['id'], '0');
+    expect(userInfoMap['name'], 'Kimmy');
+    expect(userInfoMap['email'], 'test1@gmail.com');
+    expect(userInfoMap['age'], '29');
+    expect(userInfoMap['intro'], 'love me');
+    expect(userInfoMap['gender'], 0);
+    expect(userInfoMap['avatar'], '');
+    expect(userInfoMap['city'], '');
   });
 
   // test all profile page widget
@@ -234,20 +241,10 @@ void main() {
 
   // test all profile page widget - has users
   testWidgets('AllProfilePage: one user Test', (WidgetTester tester) async {
-    UserInfoModel user = UserInfoModel(
-        id: '0',
-        name: 'Kimmy',
-        email: 'test1@gmail.com',
-        age: '29',
-        intro: 'love me',
-        gender: 0,
-        avatar: '',
-        city: '',
-        lat: 0.0,
-        lng: 0.0
-    );
+    UserInfoModel user = getMockUserInfo();
+
     when(mockFireStoreService.getUsers()).thenAnswer((_) async => Future.value([user]));
-    when(mockFireStoreService.getCurrentUid()).thenAnswer((_) => "0");
+    when(mockFireStoreService.getCurrentUid()).thenAnswer((_) => "1");
 
     await tester.pumpWidget(
         MultiProvider(
@@ -266,16 +263,86 @@ void main() {
     expect(find.text('Kimmy'), findsOneWidget);
     expect(find.text('29'), findsOneWidget);
     expect(find.byType(AllProfilesPage), findsOneWidget);
-    expect(find.byType(PageView), findsNothing);
+    expect(find.byType(PageView), findsOneWidget);
     expect(find.byType(Card), findsOneWidget);
     expect(find.byType(Column), findsOneWidget);
     expect(find.byType(Image), findsOneWidget);
 
 
     // // tap the profile
-    await tester.tap(find.byType(Card));
+    // await tester.tap(find.byType(Card));
+    // await tester.pumpAndSettle();
+    // expect(find.byType(UserProfilePage), findsOneWidget);
+    // expect(find.byType(UserProfile), findsOneWidget);
+  });
+
+  testWidgets('load map page', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+        home: MapPage(type: 'me', center: LatLng(0.0, 0.0),)
+    ));
+    expect(find.byType(MapPage), findsOneWidget);
+  });
+
+  testWidgets('widget profile page', (WidgetTester tester) async {
+    UserInfoModel user = getMockUserInfo();
+
+    when(mockFireStoreService.getUserInfo('0')).thenAnswer((_) async => Future.value(user));
+    when(mockFireStoreService.getCurrentUid()).thenAnswer((_) => "0");
+
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => NavNotifier(),
+          ),
+          Provider(create: (context) => mockFireStoreService),
+        ],
+        child: const MaterialApp(home: UserProfile(pageType: 'me', uid: '',)),
+      )
+    );
     await tester.pumpAndSettle();
-    expect(find.byType(UserProfilePage), findsOneWidget);
+
+    expect(find.text('Kimmy'), findsOneWidget);
     expect(find.byType(UserProfile), findsOneWidget);
+  });
+
+  testWidgets('edit profile page', (WidgetTester tester) async {
+    UserInfoModel user = getMockUserInfo();
+
+    when(mockFireStoreService.getCurrentUid()).thenAnswer((_) => "0");
+    when(mockFireStoreService.getUserInfo('0')).thenAnswer((_) async => Future.value(user));
+
+    await tester.pumpWidget(
+        MultiProvider(
+          providers: [
+            Provider(create: (context) => mockFireStoreService),
+          ],
+          child: const MaterialApp(home: EditProfilePage()),
+        )
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.text('Kimmy'), findsOneWidget);
+    expect(find.byType(EditProfilePage), findsOneWidget);
+  });
+
+  testWidgets('user profile page', (WidgetTester tester) async {
+    UserInfoModel user = getMockUserInfo();
+
+    when(mockFireStoreService.getCurrentUid()).thenAnswer((_) => "0");
+    when(mockFireStoreService.getUserInfo('0')).thenAnswer((_) async => Future.value(user));
+
+    await tester.pumpWidget(
+        MultiProvider(
+          providers: [
+            Provider(create: (context) => mockFireStoreService),
+          ],
+          child: const MaterialApp(home: UserProfilePage(uid: '0',)),
+        )
+    );
+    await tester.pumpAndSettle();
+
+    expect(find.text('Kimmy'), findsOneWidget);
+    expect(find.byType(UserProfilePage), findsOneWidget);
   });
 }
