2ebabb8b31772676f939ec78f889bbc9b4e12c5e
Sizhe Liu <sliu1@seattleu.edu>
2024-03-09 23:08:23 -0800

    updated logic to load avatar and name in message_list_page.dart

--- lib/message_list_page.dart
+++ lib/message_list_page.dart
@@ -51,11 +51,60 @@ class LikesPanel extends StatelessWidget {
 class MessageList extends StatelessWidget {
   const MessageList({super.key});
 
-  Widget _imageWidget(avatar) {
-    if (avatar != '') {
-      return Image.network(avatar, width: 60, height: 60, fit: BoxFit.cover,);
-    }
-    return Image.asset('assets/default.jpg', width: 60, height: 60, fit: BoxFit.cover,);
+  Future<String> getUserNameById(BuildContext context, String id) async {
+    String name = await context.read<FireStoreService>().getUserNameById(id);
+    return name;
+  }
+
+  Future<String> getUserAvatarById(BuildContext context, String id) async {
+    String avatar = await context.read<FireStoreService>().getUserAvatarById(id);
+    return avatar;
+  }
+
+  Widget _imageWidget(BuildContext context, String id) {
+    return FutureBuilder<String?>(
+      future: getUserAvatarById(context, id),
+      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // While the data is being fetched, display a loading indicator
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          // If an error occurs, display an error message
+          return Text('Error: ${snapshot.error}');
+        } else if (snapshot.hasData) {
+          // If the data is available
+          String avatar = snapshot.data!;
+          if (avatar != '') {
+            return Image.network(avatar, width: 60, height: 60, fit: BoxFit.cover,);
+          }
+          return Image.asset('assets/default.jpg', width: 60, height: 60, fit: BoxFit.cover,);
+        } else {
+          // If no data is available, display an empty text widget
+          return Image.asset('assets/default.jpg', width: 60, height: 60, fit: BoxFit.cover,);
+        }
+      },
+    );
+  }
+
+  Widget _nameWidget(BuildContext context, String id) {
+    return FutureBuilder<String?>(
+      future: getUserNameById(context, id),
+      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // While the data is being fetched, display a loading indicator
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          // If an error occurs, display an error message
+          return Text('Error: ${snapshot.error}');
+        } else if (snapshot.hasData) {
+          // If the data is available, display the user name
+          return Text(snapshot.data!);
+        } else {
+          // If no data is available, display an empty text widget
+          return const Text('');
+        }
+      },
+    );
   }
 
   @override
@@ -72,8 +121,8 @@ class MessageList extends StatelessWidget {
           }
           return ListView(
             children: queryResults.map<ListTile>((doc) => ListTile(
-              leading: ClipOval(child: _imageWidget(doc.avatar),),
-              title: Text(doc.name),
+              leading: ClipOval(child: _imageWidget(context, doc.id),),
+              title: _nameWidget(context, doc.id),
               trailing: const Icon(Icons.keyboard_arrow_right),
               contentPadding: const EdgeInsets.all(10),
               onTap: () {
